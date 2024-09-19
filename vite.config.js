c9111dfbe03614f941ec468734b3892d8b34db80
nouarsalheddine <nouarsalheddine@gmail.com>
2024-09-19 19:19:07 +0100

    dist

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   plugins: [react()],
   resolve: {
